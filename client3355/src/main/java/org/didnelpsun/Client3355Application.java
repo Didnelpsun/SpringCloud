2b08b3da6b651a7f8818605cbc3049e85b4befa6
Didnelpsun <2675350965@qq.com>
2022-04-27 23:38:41 +0800

    更新Config

---
+++ client3355/src/main/java/org/didnelpsun/Client3355Application.java
@@ -0,0 +1,12 @@
+// Client3355Application.java
+package org.didnelpsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client3355Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Client3355Application.class, args);
+    }
+}
---
+++ config3344/src/main/java/org/didnelpsun/Config3344Application.java
@@ -0,0 +1,14 @@
+// Config3344Application.java
+package org.didnelpsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config3344Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Config3344Application.class, args);
+    }
+}
