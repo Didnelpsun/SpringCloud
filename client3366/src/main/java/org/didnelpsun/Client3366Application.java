eb512bebda2bbbd695f1f367582926501814ef01
Didnelpsun <2675350965@qq.com>
2022-04-30 23:53:34 +0800

    更新

---
+++ client3366/src/main/java/org/didnelpsun/Client3366Application.java
@@ -0,0 +1,12 @@
+// Client3366Application.java
+package org.didnelpsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client3366Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Client3366Application.class, args);
+    }
+}
